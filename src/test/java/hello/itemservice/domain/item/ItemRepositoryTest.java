be96399aed49638ba04b37d3cdcc382908b08fd7
seokjw <wldn9020@daum.net>
2024-02-18 21:42:47 +0900

    [fix] 상품 도메인 개발 수정

--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -48,15 +48,17 @@ class ItemRepositoryTest {
     void updateItem() {
         // given
         Item item = new Item("item1", 10000, 10);
+
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
 
         // when
         Item updateParam = new Item("item2", 20000, 30);
         itemRepository.update(itemId, updateParam);
-        Item findItem = itemRepository.findById(itemId);
 
         // then
+        Item findItem = itemRepository.findById(itemId);
+
         assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
         assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
